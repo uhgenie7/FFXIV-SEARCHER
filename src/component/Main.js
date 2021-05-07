ac2e64289700a65391a017f16d8973e0d8a62139
uhj1993 <dzzd777@naver.com>
2021-05-07 19:34:17 +0900

    [Update] Main 컴포넌트

---
+++ src/component/Main.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Main() {
+  return (
+    <>
+      <main className="main">
+        <div className="main-container">
+          <h3>어서오세요</h3>
+          <p>
+            <strong>FFXIV SEARCHER</strong>은 REST API를 통해 JSON 형식으로
+            방대한 양의 FINAL FANTASY XIV 게임 데이터를 제공해주는
+            <strong> XIVAPI</strong>로 데이터를 받아와 프로필을 검색할 수 있는
+            사이트입니다. <br />
+            <br />
+            또한 FFXIV를 플레이하는데 도움되는 자료를 수집합니다.
+          </p>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default Main;
