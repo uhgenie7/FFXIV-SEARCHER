477448cac3dbdbab6065ae14785dfdb90fe539e5
uhj1993 <dzzd777@naver.com>
2021-05-07 19:35:34 +0900

    [Update] Loading 컴포넌트
    컴포넌트 분리

---
+++ src/component/Loading.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import load from "../img/sample/loading.gif";
+
+function Loading() {
+  return (
+    <div className="loading">
+      <img className="avatar-img" src={load} alt="avatar-img" width="25px" />
+    </div>
+  );
+}
+
+export default Loading;
