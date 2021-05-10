a73aae41bbf57d7ec1c0047092ad971e961a6576
uhj1993 <dzzd777@naver.com>
2021-05-10 20:36:45 +0900

    [Update] 배포 준비

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/ffxiv">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
