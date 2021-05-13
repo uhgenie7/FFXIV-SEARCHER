8a67b954273529c35bdfc905d8523e271105bcbb
uhj1993 <dzzd777@naver.com>
2021-05-13 18:00:22 +0900

    [Fixed] 컴포넌트 분할 및 lazy로 import 관리

--- src/App.js
+++ src/App.js
@@ -1,157 +1,62 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import "./App.scss";
-import { Link, Route, Switch } from "react-router-dom";
-import styled from "styled-components";
+import { Route, Switch } from "react-router-dom";
+// import Router
+import Home from "./routes/Home";
+import Emotes from "./routes/Emotes";
+import Emote from "./routes/Emote";
+import Another from "./routes/Another";
 // import Component
-import Main from "./component/Main";
-import Character from "./component/Character";
+import Header from "./component/Header";
+import Footer from "./component/Footer";
 import Alert from "./component/Alert";
-import Another from "./component/Another";
 import OrchIndex from "./component/Orchestrions/OrchIndex";
-import EmotesIndex from "./component/Emotes/EmotesIndex";
-import NotFound from "./component/NotFound";
+import NotFound from "./routes/NotFound";
 //import img
-import github from "./img/contact/github.png";
-import tistory from "./img/contact/tistory.png";
-
+import Loading from "./component/Loading";
+let Character = lazy(() => {
+  return import("./component/Character.js");
+});
 function App() {
   let [loading, setLoading] = useState(null);
   let [alert, setAlert] = useState(false);
-  let [nav, setNav] = useState(false);
-  let [bar, setBar] = useState(false);
-  let [side, setSide] = useState(false);
-  const SideMenuBackground = styled.div`
-    display: ${(props) => (props.side ? "block" : "none")};
-  `;
-
-  function Nav() {
-    return (
-      <ul className="navigation">
-        <li>
-          <Link to="/character">캐릭터</Link>
-        </li>
-        <li>
-          <Link to="/emotes">감정표현</Link>
-        </li>
-        <li>
-          <Link to="/orchestrions">오케스트리온</Link>
-        </li>
-        <li>
-          <Link to="/another">외부 사이트</Link>
-        </li>
-      </ul>
-    );
-  }
-
-  function SideNav() {
-    return (
-      <div className={`nav-side ${side ? "open" : ""}`}>
-        <i
-          class="fas fa-times"
-          onClick={() => {
-            setNav(false);
-            setSide(false);
-          }}
-        ></i>
-        <ul>
-          <li>
-            <Link to="/character">캐릭터</Link>
-          </li>
-          <li>
-            <Link to="/emotes">감정표현</Link>
-          </li>
-          <li>
-            <Link to="/orchestrions">오케스트리온</Link>
-          </li>
-          <li>
-            <Link to="/another">외부 사이트</Link>
-          </li>
-        </ul>
-      </div>
-    );
-  }
+  let [posts, setPosts] = useState([]);
   return (
     <div className="App">
       <div className="wrap">
-        <header>
-          <div className="header-inner">
-            <div className="nav-container">
-              <nav className="nav">
-                <Link to="/">
-                  <h1>FFXIV SEARCHER</h1>
-                </Link>
-                {nav === false ? <Nav /> : null}
-                {bar === false ? (
-                  <div
-                    className="nav-toggle"
-                    onClick={() => {
-                      setNav(true);
-                      setSide(true);
-                    }}
-                  >
-                    <i className="fas fa-bars"></i>
-                  </div>
-                ) : null}
-              </nav>
-              <SideMenuBackground
-                side={side}
-                onClick={() => {
-                  setNav(false);
-                  setSide(false);
-                }}
-              >
-                <SideNav nav={nav} />
-              </SideMenuBackground>
-            </div>
-          </div>
-        </header>
+        <Header />
         {alert === false ? <Alert setAlert={setAlert} /> : null}
         <Switch>
-          <Route exact path="/">
-            <Main />
-          </Route>
+          <Route exact path="/" component={Home} />
           <Route path="/character">
-            <Character />
+            <Suspense
+              fallback={
+                <main>
+                  <Loading />
+                </main>
+              }
+            >
+              <Character />
+            </Suspense>
           </Route>
-          <Route path="/another">
-            <Another />
-          </Route>
-          <Route path="/emotes">
-            <EmotesIndex loading={loading} setLoading={setLoading} />
-          </Route>
+          <Route
+            exact
+            path="/emotes"
+            render={() => <Emotes posts={posts} setPosts={setPosts} />}
+          />
+          <Route
+            path="/emotes/:id"
+            render={(props) => (
+              <Emote posts={posts} setPosts={setPosts} {...props} />
+            )}
+          />
           <Route path="/orchestrions">
             <OrchIndex loading={loading} setLoading={setLoading} />
           </Route>
-          <Route path="/:id">
-            <NotFound />
-          </Route>
+          <Route path="/another" component={Another} />
+          <Route component={NotFound} />
         </Switch>
-        <footer>
-          <div className="inner">
-            FINAL FANTASY XIV © 2010 - 2020 SQUARE ENIX CO., LTD. All Rights
-            Reserved.
-            <br />
-            본 사이트는 Square Enix 및 Actoz Soft와 관련이 없습니다.
-            <br />
-            ALL FINAL FANTASY XIV CONTENT IS PROPERTY OF SQUARE ENIX CO., LTD
-          </div>
-          <div className="sns">
-            <a
-              href="https://github.com/uhj1993/lodestone"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={github} alt="githube" />
-            </a>
-            <a
-              href="https://jineecode.tistory.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={tistory} alt="tistory" />
-            </a>
-          </div>
-        </footer>
+        <Footer />
       </div>
     </div>
   );
