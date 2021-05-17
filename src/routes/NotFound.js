fb9ef812d5fde40467d43f5aa0cb6d8cb04f75eb
uhj1993 <dzzd777@naver.com>
2021-05-17 18:45:00 +0900

    [Update] 404 페이지 업데이트

---
+++ src/routes/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <main>
+      <h2 className="main-title">Not found</h2>
+      <div className="main-container">
+        <h3>이 페이지에 아무런 정보가 없습니다.</h3>
+      </div>
+    </main>
+  );
+};
+
+export default NotFound;
